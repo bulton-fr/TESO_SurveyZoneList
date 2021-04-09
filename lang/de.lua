38a9d2875d5ca5a1630e46516982ec88679bbe74
bulton-fr <bulton.fr@gmail.com>
2021-04-09 04:32:37 +0200

    Translations : add DE and RU

---
+++ lang/de.lua
@@ -0,0 +1,22 @@
+-- ENGLISH LANGUAGE LOCALIZATION
+
+-- Collect
+SurveyZoneList.lang.collectFindName = {
+    ".*: (.*)",
+}
+
+SurveyZoneList.lang.surveyAction = {
+    "Hacken",
+    "Nehmen",
+    "Abbauen",
+    "Sammeln"
+}
+
+SurveyZoneList.lang.surveyType = {
+    clothier        = {"clothier"},
+    reagent         = {"alchemist"},
+    enchanting      = {"enchanter"},
+    blacksmithing   = {"blacksmith"},
+    jewerlycrafting = {"jewelry"},
+    woodworking     = {"woodworker"}
+}
---
+++ lang/ru.lua
@@ -0,0 +1,23 @@
+-- ENGLISH LANGUAGE LOCALIZATION
+
+-- Collect
+SurveyZoneList.lang.collectFindName = {
+    "карта сокровищ (.*)", --treasure map
+    ".*: (.*)",
+}
+
+SurveyZoneList.lang.surveyAction = {
+    "Рубить",
+    "Взять",
+    "Собрать",
+    "Добыть"
+}
+
+SurveyZoneList.lang.surveyType = {
+    clothier        = {"clothier"},
+    reagent         = {"alchemist"},
+    enchanting      = {"enchanter"},
+    blacksmithing   = {"blacksmith"},
+    jewerlycrafting = {"jewelry"},
+    woodworking     = {"woodworker"}
+}
